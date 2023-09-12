b9b19b81ca374bd64369d3e5b24adc40e202991e
JoachimB08 <joachimb08.kodehode@gmail.com>
2023-09-12 22:45:24 +0200

    Release

---
+++ index.js
@@ -0,0 +1,122 @@
+function myFunction7() {
+    var x = document.getElementById("dropdown");
+    if (x.style.display === "none") {
+      x.style.display = "flex";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+
+
+  function myFunction1() {
+    var x = document.getElementById("California");
+    if (x.style.display === "none") {
+      x.style.display = "flex";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function togglediv(id) {
+    document.querySelectorAll(".map1").forEach(function(div) {
+      if (div.id == id) {
+        // Toggle specified DIV
+        div.style.display = div.style.display == "none" ? "block" : "none";
+      } else {
+        // Hide other DIVs
+        div.style.display = "none";
+      }
+    });
+  }
+
+    function myFunction2() {
+    var x = document.getElementById("washington");
+    if (x.style.display === "none") {
+      x.style.display = "flex";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function togglediv1(id) {
+    document.querySelectorAll(".map2").forEach(function(div) {
+      if (div.id == id) {
+        // Toggle specified DIV
+        div.style.display = div.style.display == "none" ? "block" : "none";
+      } else {
+        // Hide other DIVs
+        div.style.display = "none";
+      }
+    });
+  }
+
+
+  function myFunction3() {
+    var x = document.getElementById("Nevada");
+    if (x.style.display === "none") {
+      x.style.display = "flex";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function togglediv2(id) {
+    document.querySelectorAll(".map3").forEach(function(div) {
+      if (div.id == id) {
+        // Toggle specified DIV
+        div.style.display = div.style.display == "none" ? "block" : "none";
+      } else {
+        // Hide other DIVs
+        div.style.display = "none";
+      }
+    });
+  }
+
+
+
+
+
+
+  function myFunction4() {
+    var x = document.getElementById("Boston");
+    if (x.style.display === "none") {
+      x.style.display = "flex";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function togglediv3(id) {
+    document.querySelectorAll(".map4").forEach(function(div) {
+      if (div.id == id) {
+        // Toggle specified DIV
+        div.style.display = div.style.display == "none" ? "block" : "none";
+      } else {
+        // Hide other DIVs
+        div.style.display = "none";
+      }
+    });
+  }
+
+
+
+
+
+
+
+  function myFunction5() {
+    var x = document.getElementById("westvirginia");
+    if (x.style.display === "none") {
+      x.style.display = "flex";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function togglediv4(id) {
+    document.querySelectorAll(".map5").forEach(function(div) {
+      if (div.id == id) {
+        // Toggle specified DIV
+        div.style.display = div.style.display == "none" ? "block" : "none";
+      } else {
+        // Hide other DIVs
+        div.style.display = "none";
+      }
+    });
+  }
